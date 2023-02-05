5b54fe2baa7cee3c12b32fcdd685d3890f79e131
lpalmarucci <lpalmarucci@gmail.com>
2023-02-05 19:36:09 +0100

    Connected to MongoDb Cluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { SensorsModule } from "./sensors/sensors.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { join } from "path";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [SensorsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: "mongodb",
+      url: process.env.DATABASE_URL,
+      database: process.env.DATABASE_NAME,
+      logging: true,
+      autoLoadEntities: true,
+      useNewUrlParser: true,
+      entities: [join(__dirname, "**/**.entity{.ts,.js}")],
+    }),
+    SensorsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
